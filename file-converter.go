a3d17a55c539bea6df06308b3bbae6105a2a28c4
EAS-Clark <98745819+EAS-Clark@users.noreply.github.com>
2023-09-04 14:10:45 +0100

    Update file-converter.go

--- file-converter.go
+++ file-converter.go
@@ -8,23 +8,22 @@ import (
 )
 
 func main() {
-    rootDir := "/path/to/your/directory" // Replace with your directory path
+    rootDir := "input" // Replace with your directory path
 
     err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
         if err != nil {
             return err
         }
 
-        if info.IsDir() {
-            // Check if the directory name contains %20
-            if strings.Contains(info.Name(), "%20") {
-                newDirName := strings.Replace(info.Name(), "%20", "-", -1)
-                newDirPath := filepath.Join(filepath.Dir(path), newDirName)
-                if err := os.Rename(path, newDirPath); err != nil {
-                    fmt.Printf("Error renaming %s to %s: %v\n", path, newDirPath, err)
-                } else {
-                    fmt.Printf("Renamed %s to %s\n", path, newDirPath)
-                }
+        // Check if the directory or filename contains %20
+        if strings.Contains(info.Name(), "%20") {
+            newName := strings.Replace(info.Name(), "%20", "-", -1)
+            newPath := filepath.Join(filepath.Dir(path), newName)
+
+            if err := os.Rename(path, newPath); err != nil {
+                fmt.Printf("Error renaming %s to %s: %v\n", path, newPath, err)
+            } else {
+                fmt.Printf("Renamed %s to %s\n", path, newPath)
             }
         }
         return nil
