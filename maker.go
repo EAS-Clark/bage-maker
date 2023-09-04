67baca0da1fb584d3604cc877959aa34a8bdd717
EAS-Clark <98745819+EAS-Clark@users.noreply.github.com>
2023-09-04 10:10:04 +0100

    Update maker.go

--- maker.go
+++ maker.go
@@ -1,79 +1,79 @@
 package main
 
 import (
-    "encoding/csv"
-    "fmt"
-    "os"
-    "strings"
+	"encoding/csv"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 func main() {
-    // Create a "badges" folder if it doesn't exist
-    err := os.MkdirAll("out", os.ModePerm)
-    if err != nil {
-        fmt.Println("Error creating 'badges' folder:", err)
-        return
-    }
+	// Create a "badges" folder if it doesn't exist
+	err := os.MkdirAll("badges", os.ModePerm)
+	if err != nil {
+		fmt.Println("Error creating 'badges' folder:", err)
+		return
+	}
 
-    // Open the CSV file
-    csvfile, err := os.Open("input.csv")
-    if err != nil {
-        fmt.Println("Error opening CSV file:", err)
-        return
-    }
-    defer csvfile.Close()
+	// Open the CSV file
+	csvfile, err := os.Open("input.csv")
+	if err != nil {
+		fmt.Println("Error opening CSV file:", err)
+		return
+	}
+	defer csvfile.Close()
 
-    // Parse the CSV
-    csvreader := csv.NewReader(csvfile)
-    records, err := csvreader.ReadAll()
-    if err != nil {
-        fmt.Println("Error reading CSV:", err)
-        return
-    }
+	// Parse the CSV
+	csvreader := csv.NewReader(csvfile)
+	records, err := csvreader.ReadAll()
+	if err != nil {
+		fmt.Println("Error reading CSV:", err)
+		return
+	}
 
-    // Loop through the CSV records and generate SVGs
-    for _, record := range records {
-        if len(record) != 2 {
-            fmt.Println("Invalid CSV record:", record)
-            continue
-        }
+	// Loop through the CSV records and generate SVG badges
+	for _, record := range records {
+		if len(record) != 2 {
+			fmt.Println("Invalid CSV record:", record)
+			continue
+		}
 
-        // Extract data from CSV
-        label := record[0]
-        status := record[1]
+		// Extract data from CSV
+		label := record[0]
+		status := record[1]
 
-        // Generate SVG based on the template
-        svg := generateSVG(label, status)
+		// Make a web request to Shields.io to generate the SVG badge
+		url := fmt.Sprintf("https://img.shields.io/badge/%s-%s-brightgreen.svg", label, status)
 
-        // Create the filename for the SVG
-        filename := fmt.Sprintf("out/%s-%s.svg", label, status)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Println("Error making the request:", err)
+			continue
+		}
+		defer resp.Body.Close()
 
-        // Create and write the SVG to the file
-        svgFile, err := os.Create(filename)
-        if err != nil {
-            fmt.Println("Error creating SVG file:", err)
-            continue
-        }
-        defer svgFile.Close()
+		if resp.StatusCode != http.StatusOK {
+			fmt.Println("Received a non-OK response:", resp.Status)
+			continue
+		}
 
-        _, err = svgFile.WriteString(svg)
-        if err != nil {
-            fmt.Println("Error writing SVG:", err)
-            continue
-        }
+		badgeData, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error reading response body:", err)
+			continue
+		}
 
-        fmt.Printf("Generated %s\n", filename)
-    }
-}
+		// Create the filename for the SVG badge
+		filename := fmt.Sprintf("output/%s-%s.svg", label, status)
 
-func generateSVG(label, status string) string {
-    svgTemplate := `
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="108" height="20" role="img" aria-label="{{LABEL}}: {{STATUS}}"><title>{{LABEL}}: {{STATUS}}</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="108" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="59" height="20" fill="#555"/><rect x="59" width="49" height="20" fill="#4caf50"/><rect width="108" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="305" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="490">{{LABEL}}</text><text x="305" y="140" transform="scale(.1)" fill="#fff" textLength="490">{{LABEL}}</text><text aria-hidden="true" x="825" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="390">{{STATUS}}</text><text x="825" y="140" transform="scale(.1)" fill="#fff" textLength="390">{{STATUS}}</text></g></svg>
-`
+		// Write the SVG badge to the file
+		err = ioutil.WriteFile(filename, badgeData, 0644)
+		if err != nil {
+			fmt.Println("Error writing SVG badge to file:", err)
+			continue
+		}
 
-    // Replace placeholders with actual data
-    svg := strings.ReplaceAll(svgTemplate, "{{LABEL}}", label)
-    svg = strings.ReplaceAll(svg, "{{STATUS}}", status)
-
-    return svg
+		fmt.Printf("Generated %s\n", filename)
+	}
 }
